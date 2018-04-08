dace5bc461bcec2df3d04584e794dc0e16798945
efefre <efefre@icloud.com>
2018-04-08 17:29:51 +0200

    first commit

---
+++ learning_users/basic_app/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+from django.contrib.auth.models import User
+from basic_app.models import UserProfileInfo
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+   
+    class Meta():
+        model = User
+        fields = ('username','email','password')
+
+class UserProfileInfoForm(forms.ModelForm):
+    class Meta():
+        model = UserProfileInfo
+        fields = ('portfolio_site','profile_pic')
+        labels = {
+            'portfolio_site': 'Adres strony',
+            'profile_pic': 'Zjęcie profilowe',
+        }
+
+        help_texts = {
+            'portfolio_site': 'Podaj adres z https',
+            'profile_pic': 'Wstaw zdjęcie profilowe',
+        }
+
+
